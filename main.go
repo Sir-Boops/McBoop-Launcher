e629755b5a2712a62621f8157225e4d1e96c76bf
Sir_Boops <admin@boops.me>
2019-06-07 13:19:54 -0600

    Furst commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  // TODO: Auto update this boi
+
+  // What this does
+  // 1: Creates a ~/.McBoop folder
+  // 2: Downloads latest good openjdk version
+  // ( Checking the meta.json file at https://git.sergal.org/Sir-Boops/McBoop-Launcher )
+  // 3: Downloads and puts McBoop.jar into ~/.mcboop
+  // 4: IF McBoop.jar was already present then just check for updates for it and auto update
+
+  fmt.Println("Gay")
+}
